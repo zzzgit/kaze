6449ec8dcef2ea20594598aa6918dc596b655616
jonkee <zzzgoo@gmail.com>
2022-02-13 10:45:34 +0800

    chore: update dependency

--- .huskyrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	"hooks": {
-		"pre-push": "env FORCE_COLOR=1 npm test",
-		'prepare-commit-msg': 'echo $HUSKY_GIT_PARAMS',
-		"commit-msg": "env FORCE_COLOR=1 commitlint -E HUSKY_GIT_PARAMS"
-	}
-}
